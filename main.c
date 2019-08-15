8b355962a5ef3f0632a137e72f1c35218f0da1d3
LuoZhongYao <LuoZhongYao@gmail.com>
2019-08-15 10:14:17 +0800

    support h5

--- main.c
+++ main.c
@@ -30,6 +30,8 @@
 #define HCI_MAX_EVENT_SIZE   260
 #define HCI_MAX_FRAME_SIZE  (HCI_MAX_ACL_SIZE + 4)
 static const uint64_t BTSNOOP_EPOCH_DELTA = 0x00dcddb30f2f8000ULL;
+static bool use_h5 = false;
+//static bool use_h5_btsnoop = false;
 
 static const struct h4_pkt_match {
     uint8_t type;
@@ -83,7 +85,10 @@ static int btsnoop_create(const char *file)
 		perror(file);
 		exit(1);
 	}
-	write(fd, "btsnoop\0\0\0\0\1\0\0\x3\xea", 16);
+	if (use_h5)
+		write(fd, "btsnoop\0\0\0\0\1\0\0\x3\xec", 16);
+	else
+		write(fd, "btsnoop\0\0\0\0\1\0\0\x3\xea", 16);
 	return fd;
 }
 
@@ -99,6 +104,31 @@ static uint64_t btsnoop_timestamp(void)
   return timestamp;
 }
 
+static void btsnoop_write(int fd, const uint8_t *buf, unsigned length, bool is_received)
+{
+	int flags;
+	int drops = 0;
+	int length_he = 0;
+	uint64_t timestamp = btsnoop_timestamp();
+	uint32_t time_hi = timestamp >> 32;
+	uint32_t time_lo = timestamp & 0xFFFFFFFF;
+
+	length_he = htonl(length);
+	flags = htonl(is_received);
+	drops = htonl(drops);
+	time_hi = htonl(time_hi);
+	time_lo = htonl(time_lo);
+
+	write(fd, &length_he, 4);
+	write(fd, &length_he, 4);
+	write(fd, &flags, 4);
+	write(fd, &drops, 4);
+	write(fd, &time_hi, 4);
+	write(fd, &time_lo, 4);
+	write(fd, buf, length);
+}
+
+#if 0
 static void btsnoop_write(int fd, const uint8_t *buf, bool is_received)
 {
 	int length;
@@ -147,6 +177,7 @@ static void btsnoop_write(int fd, const uint8_t *buf, bool is_received)
 	write(fd, &type, 1);
 	write(fd, packet, length_he - 1);
 }
+#endif
 
 static speed_t tty_get_speed(int speed)
 {
@@ -182,6 +213,9 @@ static int uart_open(const char *dev, int speed)
 		exit(1);
 	}
 
+	if (!isatty(fd))
+		return fd;
+
 	tcflush(fd, TCIOFLUSH);
 
 	if (tcgetattr(fd, &ti) < 0) {
@@ -191,9 +225,12 @@ static int uart_open(const char *dev, int speed)
 	cfmakeraw(&ti);
 	ti.c_cflag |= CLOCAL;
 	ti.c_cflag &= ~CRTSCTS;
-	ti.c_cflag &= ~PARENB;
 	ti.c_cflag &= ~CSTOPB;
 	ti.c_cflag |= CS8;
+	if (use_h5)
+		ti.c_cflag |= PARENB;
+	else
+		ti.c_cflag &= ~PARENB;
 	ti.c_cc[VMIN] = 1;
 	cfsetispeed(&ti, tty_get_speed(speed));
 	cfsetospeed(&ti, tty_get_speed(speed));
@@ -214,10 +251,13 @@ struct context
 	bool in;
 	int btsnoop;
 	struct pt pt;
+	unsigned flags;
 	unsigned curn;
 	unsigned readn;
+	unsigned h5n;
 	struct frame frm;
 	unsigned char buf[65535];
+	unsigned char h5b[65535];
 };
 
 #define READ_BYTE(h4, buf, length) \
@@ -263,7 +303,7 @@ static PT_THREAD(h4_process(struct context *c, void *buf, unsigned size))
 		READ_BYTE(c, buf, size);
 
 		if (c->buf[0] > HCI_EVENT_PKT) {
-			fprintf(stderr, "Invalid package type: %02x\n", c->buf[0]);
+			fprintf(stderr, "(%s) Invalid package type: %02x\n", c->in ? "RX" : "TX", c->buf[0]);
 			continue;
 		}
 
@@ -276,7 +316,7 @@ static PT_THREAD(h4_process(struct context *c, void *buf, unsigned size))
 		READ_BYTE(c, buf, size);
 		dlen = h4_pkt_len(c->buf, h4_pkts + c->buf[0]);
         if (dlen > HCI_MAX_FRAME_SIZE) {
-			fprintf(stderr, "Invalid packet\n");
+			fprintf(stderr, "(%s) Invalid packet\n", c->in ? "RX" : "TX");
             continue;
 		}
 
@@ -284,7 +324,7 @@ static PT_THREAD(h4_process(struct context *c, void *buf, unsigned size))
         READ_BYTE(c, buf, size);
 
 		if (c->btsnoop != -1) {
-			btsnoop_write(c->btsnoop, c->buf, c->in);
+			btsnoop_write(c->btsnoop, c->buf, c->readn, c->in);
 		}
 
 		c->frm.in = c->in;
@@ -292,6 +332,181 @@ static PT_THREAD(h4_process(struct context *c, void *buf, unsigned size))
 		c->frm.ptr = c->frm.data;
 		c->frm.len = c->frm.data_len;
 		hci_dump(0, &c->frm);
+	}
+
+	PT_END(&c->pt);
+}
+#undef READ_BYTE
+
+#define H5_HDR_SEQ(hdr)		((hdr)[0] & 0x07)
+#define H5_HDR_ACK(hdr)		(((hdr)[0] >> 3) & 0x07)
+#define H5_HDR_CRC(hdr)		(((hdr)[0] >> 6) & 0x01)
+#define H5_HDR_RELIABLE(hdr)	(((hdr)[0] >> 7) & 0x01)
+#define H5_HDR_PKT_TYPE(hdr)	((hdr)[1] & 0x0f)
+#define H5_HDR_LEN(hdr)		((((hdr)[1] >> 4) & 0x0f) + ((hdr)[2] << 4))
+
+#define HCI_3WIRE_ACK_PKT   0
+#define HCI_3WIRE_LINK_PKT 15
+
+#define SLIP_DELIMITER	0xc0
+#define SLIP_ESC	0xdb
+#define SLIP_ESC_DELIM	0xdc
+#define SLIP_ESC_ESC	0xdd
+
+#define H5_RX_ESC		0x01
+#define H5_TX_ACK_REQ	0x02
+#define H5_LINK_FLAG(n)	(1 << (n - 1))
+
+static inline int unslip_one_byte(struct context *c, uint8_t ch)
+{
+	uint8_t byte = ch;
+	if (!(c->flags & H5_RX_ESC) && ch == SLIP_ESC) {
+		c->flags |= H5_RX_ESC;
+		return 0;
+	}
+	
+	if (ch == SLIP_DELIMITER)
+		goto _err;
+
+	if (c->flags & H5_RX_ESC) {
+		c->flags &= ~H5_RX_ESC;
+		switch (ch) {
+		case SLIP_ESC_DELIM:
+			byte = SLIP_DELIMITER;
+		break;
+
+		case SLIP_ESC_ESC:
+			byte = SLIP_ESC;
+		break;
+
+		default:
+_err:
+			fprintf(stderr, "(%s) unslip byte error: %02x\n", c->in ? "RX" : "TX", ch);
+			return 1;
+		break;
+		}
+	}
+	c->buf[c->curn++] = byte;
+	return 0;
+}
+
+static inline void hci_3wire_recv_frame(struct context *c)
+{
+	const uint8_t *data;
+	switch (H5_HDR_PKT_TYPE(c->buf)) {
+	case HCI_COMMAND_PKT: 
+		c->frm.in = false;
+		goto frmdump;
+
+	case HCI_EVENT_PKT:
+		c->frm.in = false;
+		goto frmdump;
+
+	case HCI_ACLDATA_PKT:
+	case HCI_SCODATA_PKT:
+frmdump:
+		c->buf[3] = H5_HDR_PKT_TYPE(c->buf);
+
+        c->frm.in = c->in;
+        c->frm.data_len = c->readn - 3;
+		c->frm.data = c->buf + 3;
+        c->frm.ptr = c->frm.data;
+        c->frm.len = c->frm.data_len;
+        hci_dump(0, &c->frm);
+	break;
+
+	case HCI_3WIRE_ACK_PKT:
+		printf("[%s] 3wire ack pkt\n", c->in ? "RX" : "TX");
+	break;
+
+	case HCI_3WIRE_LINK_PKT:
+		data = c->buf + 4;
+		if (data[0] == 0x01 && data[1] == 0x7e) { /* sync req */
+			printf("[%s] 3wire sync req\n", c->in ? "RX" : "TX");
+		} else if (data[0] == 0x02 && data[1] == 0x7d) {	/* sync rsp */
+			printf("[%s] 3wire sync rsp\n", c->in ? "RX" : "TX");
+		} else if (data[0] == 0x03 && data[1] == 0xfc) {	/* conf req */
+			printf("[%s] 3wire conf req\n", c->in ? "RX" : "TX");
+		} else if (data[0] == 0x04 && data[1] == 0x7b) {	/* conf rsp */
+			printf("[%s] 3wire conf rsp\n", c->in ? "RX" : "TX");
+		} else if (data[0] == 0x05 && data[1] == 0xfa) {	/* sleep req */
+			printf("[%s] 3wire sleep req\n", c->in ? "RX" : "TX");
+		} else if (data[0] == 0x06 && data[1] == 0xf9) {	/* woken req */
+			printf("[%s] 3wire woken req\n", c->in ? "RX" : "TX");
+		} else if (data[0] == 0x07 && data[1] == 0x78) {	/* wakeup req */
+			printf("[%s] 3wire wakeup req\n", c->in ? "RX" : "TX");
+		}
+	break;
+	}
+}
+#define unslip_one_byte(c, ch) ({int var = unslip_one_byte(c, ch); if(var) printf("(c = %d, r = %d)-----> %s:%d\n", c->curn, c-> readn, __func__, __LINE__); var; })
+
+#define READ_BYTE(c, buf, length) \
+		do { \
+			while(c->curn < c->readn) { \
+				uint8_t ch;\
+				PT_WAIT_UNTIL(&c->pt, length > 0);\
+				ch = *buf++; \
+				length--;\
+				c->h5b[c->h5n++] = ch;\
+				if (unslip_one_byte(c, ch) == 1) \
+					goto again; \
+			}\
+		} while(0)
+
+
+static PT_THREAD(h5_process(struct context *c, const uint8_t *buf, unsigned size))
+{
+	uint8_t ch;
+	PT_BEGIN(&c->pt);
+
+	while (1) {
+again:
+		do {
+			PT_WAIT_UNTIL(&c->pt,  size > 0);
+			size--;
+		} while (*buf++ != SLIP_DELIMITER);
+
+		do {
+			PT_WAIT_UNTIL(&c->pt, size > 0);
+			size--;
+		} while ((ch = *buf++) == SLIP_DELIMITER);
+
+		c->flags &= ~H5_RX_ESC;
+
+		c->readn = 4;
+		c->curn = 0;
+		c->h5n = 0;
+
+		c->h5b[c->h5n++] = ch;
+		if (unslip_one_byte(c, ch) == 1)
+			goto again;
+
+		READ_BYTE(c, buf, size);
+
+		if (((c->buf[0] + c->buf[1] + c->buf[2] + c->buf[3]) & 0xff) != 0xff) {
+			fprintf(stderr, "(%s) header crc error\n", c->in ? "RX" : "TX");
+			goto again;
+		}
+
+		c->readn += H5_HDR_LEN(c->buf);
+		READ_BYTE(c, buf, size);
+		if (H5_HDR_CRC(c->buf)) {
+			c->readn += 2;
+			READ_BYTE(c, buf, size);
+			c->readn -= 2;
+		}
+
+		//printf("(%s) seq = %d, ack = %d, %sreliable\n",
+		//	c->in ? "RX" : "TX", H5_HDR_SEQ(c->buf), H5_HDR_ACK(c->buf), H5_HDR_RELIABLE(c->buf)?"":"un");
+		//if (H5_HDR_RELIABLE(c->buf)) {
+		//}
+
+		hci_3wire_recv_frame(c);
+
+		if (c->btsnoop != -1) {
+			btsnoop_write(c->btsnoop, c->h5b, c->h5n, c->in);
+		}
 	}
 
 	PT_END(&c->pt);
@@ -311,10 +526,10 @@ static void context_init(struct context *c, int fd, int btsnoop, bool in)
 static int process_frames(int tx, int rx, int btsnoop)
 {
 	int nfds = 0;
-	unsigned char buf[1024];
 	struct pollfd fds[2];
 	struct context *ctx[2];
 	struct context *c;
+	unsigned char buf[1024];
 
 	ctx[0] = calloc(sizeof(struct context), 1);
 	ctx[1] = calloc(sizeof(struct context), 1);
@@ -355,7 +570,10 @@ static int process_frames(int tx, int rx, int btsnoop)
 				rn = read(fd, buf, sizeof(buf));
 				if (rn <= 0)
 					continue;
-				h4_process(c, buf, rn);
+				if (use_h5)
+					h5_process(c, buf, rn);
+				else
+					h4_process(c, buf, rn);
 			}
 		}
 	}
@@ -373,7 +591,7 @@ quit:
 
 static void usage(void)
 {
-	printf("usage: btuart -w {btsnoop} -r {rx} -t {tx} -b {baudrate} -h\n");
+	printf("usage: btuart -w {btsnoop} -r {rx} -t {tx} -b {baudrate} -5 -h\n");
 	exit(1);
 }
 
@@ -383,12 +601,13 @@ int main(int argc, char **argv)
 	unsigned long flags = 0, filter = 0;
 	const char *rx_file = NULL, *tx_file = NULL, *btsnoop_file = NULL;
 
-	while (-1 != (c = getopt(argc, argv, "r:t:b:w:h"))) {
+	while (-1 != (c = getopt(argc, argv, "r:t:b:w:h5"))) {
 		switch (c) {
 			 case 'r': rx_file = optarg; break;
 			 case 't': tx_file = optarg; break;
 			 case 'b': speed = strtol(optarg, NULL, 0); break;
 			 case 'w': btsnoop_file = optarg; break;
+			 case '5': use_h5 = true; break;
 			 case 'h':
 			 default:
 				usage();
